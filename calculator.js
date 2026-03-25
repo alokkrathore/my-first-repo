caafc88fc9684602f814c3b7b534b345e0a56cf3
alok rathore <alokkrathore@gmail.com>
2026-03-25 23:00:35 +0530

    trying to sove syntax issue

--- calculator.js
+++ calculator.js
@@ -1,3 +1,3 @@
-echo 'function add(a, b) { return a + b +1; }
+echo 'function add(a, b) { return a + b + 1; }
 function multiply(a, b) { return a * b; }
 module.exports = { add, multiply };' > calculator.js
